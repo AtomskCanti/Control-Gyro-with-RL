9471284bc86d47e6730f5d20671f0e3c5c39aa62
XingXing <zhitaohuang@xingxingdebugshengchengqi.local>
2021-01-28 14:20:32 +0100

    Delete old files

--- code/gym_gyroscope_env/gym_gyroscope_env/envs/__init__.py
+++
@@ -1,4 +0,0 @@
-from gym_gyroscope_env.envs.gyroscope_env import GyroscopeEnv
-from gym_gyroscope_env.envs.gyroscope_discontinuous_env import GyroscopeDiscontinuousEnv
-from gym_gyroscope_env.envs.gyroscope_integral_env import GyroscopeIntegralEnv
-from gym_gyroscope_env.envs.gyroscope_robust_env import GyroscopeRobustEnv
